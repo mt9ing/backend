29e18725c969eef88e09af50ed0cf565de7db2a2
mt9ing <mt9ing@gmail.com>
2025-04-16 19:46:43 +0200

    Changed The Database check log message

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,6 @@ const PORT = 5000;
 (async () => {
   const db = await dbConnection();
   await db.exec(`CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, title TEXT NOT NULL)`);
-  console.log("DB created");  
+  console.log("Database connected and table created");  
   app.listen(PORT, () => console.log(`http://localhost:${PORT}`))
 })()
